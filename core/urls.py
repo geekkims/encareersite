2b37e9e5b65908c2a1a37c01068a744a8cf34bc6
James Kimamo <james.kimamo@green.earth>
2025-05-30 08:12:07 +0300

    resolved gitignore

--- core/urls.py
+++ core/urls.py
@@ -5,15 +5,20 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('authentication/',include('authentication.urls')),
-    path('',include('website.urls')),
-    path('',include('jobapp.urls')),
-    path('',include('contactus.urls')),
-    path('blogs/',include('blogs.urls')),
+    path('authentication/', include('authentication.urls')),
+    path('', include('website.urls')),
+    path('', include('jobapp.urls')),
+    path('', include('contactus.urls')),
+    path('blogs/', include('blogs.urls')),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')), # The CKEditor path For Support
-
 ]
 
-
-urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# Fix the static files serving for development
+if settings.DEBUG:
+    # For development - use STATICFILES_DIRS
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # For production - use STATIC_ROOT
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jobapp/views.py
+++ jobapp/views.py
@@ -8,57 +8,92 @@ from django.http import Http404, HttpResponseRedirect, JsonResponse, HttpRespons
 
 # Create your views here.
 def index(request):
-    logo_obj= Mainlogo.objects.last() 
-    job_obj=Job.objects.filter(is_closed=False)
-    photo=Jobphoto.objects.last()
-    services_obj=Service.objects.filter(status=True)
+    try:
+        logo_obj = Mainlogo.objects.last()
+    except:
+        logo_obj = None
+        
+    job_obj = Job.objects.filter(is_closed=False)
+    
+    try:
+        photo = Jobphoto.objects.last()
+    except:
+        photo = None
+        
+    try:
+        services_obj = Service.objects.filter(status=True)
+    except:
+        services_obj = []
 
-
-    context={
-
-        "logo_obj":logo_obj,
-        "job_obj":job_obj,
-        "photo":photo,
-        "services_obj":services_obj,
-
+    context = {
+        "logo_obj": logo_obj,
+        "job_obj": job_obj,
+        "photo": photo,
+        "services_obj": services_obj,
     }
-    return render(request,'front-end/jobs/index.html',context)
+    return render(request, 'front-end/jobs/index.html', context)
 
 
-def JobDetails(request,slug):
-    logo_obj= Mainlogo.objects.last() 
-    single_job=Job.objects.get(slug=slug)
-    services_obj=Service.objects.filter(status=True)
-    photo=Jobphoto.objects.last()
+def JobDetails(request, slug):
+    try:
+        logo_obj = Mainlogo.objects.last()
+    except:
+        logo_obj = None
+        
+    single_job = Job.objects.get(slug=slug)
+    
+    try:
+        services_obj = Service.objects.filter(status=True)
+    except:
+        services_obj = []
+        
+    try:
+        photo = Jobphoto.objects.last()
+    except:
+        photo = None
 
-    context={
-
-        'logo_obj':logo_obj,
-        'single_job':single_job,
-        "services_obj":services_obj,
-        "photo":photo,
-
+    context = {
+        'logo_obj': logo_obj,
+        'single_job': single_job,
+        "services_obj": services_obj,
+        "photo": photo,
     }
 
-
-
-    return render(request,'front-end/jobs/detail.html',context)
-
+    return render(request, 'front-end/jobs/detail.html', context)
 
 
 class JobDetailsView(DetailView):
     model = Job
     template_name = "jobs/details.html"
     context_object_name = "job"
-    logo_obj= Mainlogo.objects.last() 
     pk_url_kwarg = "id"
 
     def get_object(self, queryset=None):
         obj = super(JobDetailsView, self).get_object(queryset=queryset)
-        logo_obj= Mainlogo.objects.last() 
         if obj is None:
             raise Http404("Job doesn't exists")
         return obj
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Move database queries here instead of class level
+        try:
+            context['logo_obj'] = Mainlogo.objects.last()
+        except:
+            context['logo_obj'] = None
+            
+        try:
+            context['services_obj'] = Service.objects.filter(status=True)
+        except:
+            context['services_obj'] = []
+            
+        try:
+            context['photo'] = Jobphoto.objects.last()
+        except:
+            context['photo'] = None
+            
+        return context
 
     def get(self, request, *args, **kwargs):
         try:
@@ -68,6 +103,3 @@ class JobDetailsView(DetailView):
             raise Http404("Job doesn't exists")
         context = self.get_context_data(object=self.object)
         return self.render_to_response(context)
-
-
-
